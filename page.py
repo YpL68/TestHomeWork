3cfe962940e561d0b673c34416bd8269cf34f3cf
YpL68 <yuriy.plks@gmail.com>
2024-03-16 11:24:38 +0200

    Page modele

---
+++ page.py
@@ -0,0 +1,32 @@
+from appium.webdriver.common.appiumby import AppiumBy
+from appium.webdriver.webdriver import WebDriver
+from appium.webdriver.webelement import WebElement
+
+
+class Page:
+    def __init__(self, driver: WebDriver, name_page: str):
+        self.driver = driver
+        self.name_page = name_page
+
+    def find_element(self, xpath: str) -> WebElement | None:
+        try:
+            result = self.driver.find_element(AppiumBy.XPATH, xpath)
+        except Exception:
+            result = None
+        return result
+
+    def find_and_click_element(self, xpath: str) -> bool:
+        try:
+            element = self.find_element(xpath)
+            element.click()
+            return True
+        except Exception:
+            return False
+
+    def find_and_send_keys_element(self, xpath: str, value: str) -> bool:
+        try:
+            element = self.find_element(xpath)
+            element.send_keys(value)
+            return True
+        except Exception:
+            return False
